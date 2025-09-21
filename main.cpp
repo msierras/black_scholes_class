01a1f528d478985c2c7ab4e87de284a3a3664462
Msierra001 <melvin.sierraic3@gmail.com>
2025-09-21 19:42:12 -0400

    Adding comments to BlackScholes::operator()(double vol)
    Addition of clarifying comments to BlackScholes::operator()(double vol)

--- main.cpp
+++ main.cpp
@@ -35,22 +35,30 @@ double BlackScholes::operator()(double vol){
 
     const int phi = static_cast<int>(payoff_type_); // recovering the value of phi by casting the payoff type enumerator to its constant integer equivalent
 
+    // The non-trivial case for pricing an option is before it has expired (time_to_exp_ > 0.0). In this case, the Black-Scholes formula is applied.
     if(time_to_exp_ > 0.0){
+        // Calculating the values of d1 and d2, which will be used as arguments in the standard normal CDF
         auto norm_args = compute_norm_args_(vol);
+
+        // d1 and d2 are stored in the array<double, 2> container norm_args and accessed by using the usual square bracket operator. 
         double d1 = norm_args[0];
         double d2 = norm_args[1];
 
+        // We get the N(d1) and N(d2) values via a lambda expression that invokes the error function erf(.)  
         auto norm_cdf = [](double x){
             return (1.0 + std::erf(x / std::numbers::sqrt2)) / 2.0;
         };
 
+        // Calculating nd_1 and nd_2 via the norm_cdf lambda expression we declared above 
         double nd_1 = norm_cdf(phi * d2);   // N(d1)
         double nd_2 = norm_cdf(phi * d2);   // N(d2)
+
+        // Computing the discount factor from T back to time t (time_to_exp_ = T - t) 
         double disc_fctr = exp(-rate_ * time_to_exp_);
 
-        return phi * (spot_* exp(-div_ * time_to_exp_) * nd_1 - disc_fctr * strike_ * nd_2);
+        return phi * (spot_* exp(-div_ * time_to_exp_) * nd_1 - disc_fctr * strike_ * nd_2); 
     }
-    else{
+    else{   // In the event the option is expired, its value is simply the raw payoff
         return std::max(phi * (spot_ - strike_), 0.0);
     }
 
